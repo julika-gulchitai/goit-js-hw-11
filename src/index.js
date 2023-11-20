6b42f1dad8668193847905a2d64fefaff378c138
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2023-11-20 12:23:55 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -16,11 +16,17 @@ let searchSubject = '';
 const onGetQuerySubmit = async event => {
   event.preventDefault();
   const { target: form } = event;
-  // if (event.currentTarget.elements.searchQuery.value !== '') form.reset;
-  console.log(event.currentTarget.elements.searchQuery.value);
+
+  if (event.currentTarget.elements.searchQuery.value.trim() == '') {
+    form.reset();
+    gallery.innerHTML = '';
+    loadMore.classList.add('is-hidden');
+    Notiflix.Notify.failure('Sorry! Empty request. Please, try again...');
+    return;
+  }
+
   searchSubject = event.currentTarget.elements.searchQuery.value.trim();
   pixabay.q = searchSubject;
-
   pixabay.page = 1;
   try {
     const { data } = await pixabay.getPhotosByQuery(searchSubject);
@@ -77,6 +83,6 @@ const onLoadmorePhoto = async () => {
     Notiflix.Notify.failure('Sorry! Invalid request. Something went wrong...');
   }
 };
+
 form.addEventListener('submit', onGetQuerySubmit);
 loadMore.addEventListener('click', onLoadmorePhoto);
-searchSubject = '';
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,3 @@
 export const form = document.querySelector('.search-form');
 export const gallery = document.querySelector('.gallery');
 export const loadMore = document.querySelector('.load-more');
-export const bigPhoto = document.querySelector('.gallery-img');
